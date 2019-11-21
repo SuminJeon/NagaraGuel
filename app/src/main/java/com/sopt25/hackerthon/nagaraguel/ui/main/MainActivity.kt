1c6a930eb3ada824fc8fe93f32b0fafafdca6e3e
CHJUNE-android <chjune0205@gmail.com>
2019-11-22 01:38:35 +0900

    로그인 및 회원가입 구현 + 뷰페이저

--- app/src/main/java/com/sopt25/hackerthon/nagaraguel/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt25/hackerthon/nagaraguel/ui/main/MainActivity.kt
@@ -2,12 +2,34 @@ package com.sopt25.hackerthon.nagaraguel.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.LinearSnapHelper
-import androidx.recyclerview.widget.SnapHelper
+import android.view.MenuItem
+import android.view.View
+import androidx.core.view.get
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sopt25.hackerthon.nagaraguel.R
+import com.sopt25.hackerthon.nagaraguel.ui.bottom_nav.HomeFragment
+import com.sopt25.hackerthon.nagaraguel.ui.bottom_nav.MyTotalFragment
+import com.sopt25.hackerthon.nagaraguel.ui.bottom_nav.RankFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    lateinit var viewPager_main : ViewPager
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.navigation_home ->{
+                setThisPage(p0,1)
+            }
+            R.id.navigation_my_total -> {
+                setThisPage(p0,0)
+            }
+            R.id.navigation_rank -> {
+                setThisPage(p0,2)
+            }
+        }
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +37,40 @@ class MainActivity : AppCompatActivity() {
         init()
     }
     private fun init(){
+        viewPager_main = findViewById(R.id.fragment_container)
+        setUpViewPager(viewPager_main)
+        bottom_navigation_view.setOnNavigationItemSelectedListener(this)
+        val home_select = bottom_navigation_view.menu.getItem(1)
+        onNavigationItemSelected(home_select)
+        home_select.setChecked(true)
+        viewPager_main?.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
 
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+            override fun onPageSelected(position: Int) {
+                val menu_item = bottom_navigation_view.menu.getItem(position)
+                setThisPage(menu_item,position)
+            }
+
+        })
     }
+
+    private fun setThisPage(menuItem: MenuItem,postition:Int){
+        menuItem.setChecked(true)
+        viewPager_main.setCurrentItem(postition)
+    }
+
+    private fun setUpViewPager(viewPager : ViewPager){
+        val adapter = MyPagerAdapter(supportFragmentManager)
+        adapter.addFragment(MyTotalFragment())
+        adapter.addFragment(HomeFragment())
+        adapter.addFragment(RankFragment())
+        viewPager.adapter=adapter
+    }
+
 }
 
---
+++ app/src/main/java/com/sopt25/hackerthon/nagaraguel/ui/main/MyPagerAdapter.kt
@@ -0,0 +1,19 @@
+package com.sopt25.hackerthon.nagaraguel.ui.main
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    var mFragmentList = mutableListOf<Fragment>()
+
+    override fun getItem(position: Int): Fragment {
+        return mFragmentList.get(position)
+    }
+
+    override fun getCount(): Int {
+        return mFragmentList.size
+    }
+
+    fun addFragment(fragment: Fragment){mFragmentList.add(fragment)}
+}
